e3d9a12d17e602beb35d482760ca6ca7b19bce0a
gela pipia <gelapipia10@gmail.com>
2024-01-22 23:37:55 +0400

    UPDATE: added functions for getting user and post datas from DB

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,50 @@
-export const getPosts = () => {
-    
+import { PostModel } from "@/Models/PostModel";
+import { connectToDB } from "./utils";
+import { UserModel } from "@/Models/UserModel";
+
+
+export const getPosts = async () => {
+    try {
+        connectToDB();
+        const posts = await PostModel.find({});
+        if(!posts) throw new Error("DB is empty, there is not posts")
+        return posts;
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+}
+
+export const getPost = async (id: string) => {
+    try {
+        connectToDB();
+        const post = await PostModel.findById(id)
+        if(!post) throw new Error("no post found in DB")
+        return post;
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+}
+
+
+export const getUser = async (id: string) => {
+    try {
+        connectToDB();
+        const user = await UserModel.findById(id)
+        if(!user) throw new Error("user not found in DB")
+        return user;
+    } catch (error) {
+        console.log("Error: ", error)
+    }
+}
+
+
+export const getUsers = async () => {
+    try {
+        connectToDB();
+        const users = await UserModel.find({});
+        if(!users) throw new Error("DB is empty, there is no users")
+        return users;
+    } catch (error) {
+        console.log("Error: ", error)
+    }
 }
